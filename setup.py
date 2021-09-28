f9b7d7845ed9a99b084246251f180622ce04435e
rafael.ameijeiras <rafael.ameijeiras@gmail.com>
2021-09-28 14:51:54 +0200

    adding long documentation to setup.py file

--- setup.py
+++ setup.py
@@ -1,5 +1,9 @@
 from setuptools import setup, find_packages
 import os
+import sys
+
+with open('README.md', mode='r', encoding='utf-8') as f:
+    readme = f.read()
 
 # Setting up
 setup(
@@ -8,6 +12,8 @@ setup(
     author="PandoraFMS projects department",
     author_email="<projects@pandorafms.com>",
     description="A pluguin tool set of functions for pandorafms",
+    long_description=readme,
+    long_description_content_type='text/markdown',
     url = 'https://github.com/projects-pandorafms/pandoraPlugintools',
     packages=find_packages(),
     zip_safe=False,
