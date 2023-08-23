9e39abf1825ef096a2fa38605cc1ef449c5a42c6
alejandro <alejandro.sanchez@pandorafms.com>
2023-08-23 12:40:52 +0200

    fix typo

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     url = 'https://github.com/projects-pandorafms/pandoraPlugintools',
     packages=find_packages(),
     zip_safe=False,
-    install_requires=['datetime', 'cryptography==3', 'requests', 'requests_ntlm','pycrypto==2.6.1'.'easysnmp==0.2.6','pysnmp==4.4.12'],
+    install_requires=['datetime', 'cryptography==3', 'requests', 'requests_ntlm','pycrypto==2.6.1','easysnmp==0.2.6','pysnmp==4.4.12'],
     keywords=['python', 'pandora', 'pandorafms', 'plugintool', 'plugintools'],
     classifiers=[
         "Intended Audience :: Developers",
