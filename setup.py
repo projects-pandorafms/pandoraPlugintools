6fda17324f858099b1ef2e3e0e879eba437b9875
rafael <rafael.ameijeiras@pandorafms.com>
2024-04-18 11:59:41 +0200

    remove deprecated pycrypto

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('README.md', mode='r', encoding='utf-8') as f:
 # Setting up
 setup(
     name="pandoraPlugintools",
-    version="1.0.9",
+    version="1.0.10",
     author="PandoraFMS projects department",
     author_email="<projects@pandorafms.com>",
     description="A plugin tool set of functions for pandorafms",
@@ -17,8 +17,8 @@ setup(
     url = 'https://github.com/projects-pandorafms/pandoraPlugintools',
     packages=find_packages(),
     zip_safe=False,
-    install_requires=['datetime', 'cryptography>=3', 'requests', 'requests_ntlm','pycrypto>=2.6.1','pysnmp>=4.4.12','pycryptodome'],
-    keywords=['python', 'pandora', 'pandorafms', 'plugintool', 'plugintools'],
+    install_requires=['datetime', 'cryptography>=3', 'requests', 'requests_ntlm','pysnmp>=4.4.12','pycryptodome'],
+    keywords=['python', 'pandora', 'pandorafms', 'plugintool', 'plugintools', 'pandoraPlugintools'],
     classifiers=[
         "Intended Audience :: Developers",
         "Programming Language :: Python :: 3",
